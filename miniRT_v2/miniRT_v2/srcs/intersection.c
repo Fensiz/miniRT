e168e293ae5a8c05b0dad8c013f668fbde5a7d4e
Симонов Иван Дмитриевич <simon@simons-macbook.local>
2022-01-10 22:50:53 +0300

    test

--- miniRT_v2/miniRT_v2/srcs/intersection.c
+++ miniRT_v2/miniRT_v2/srcs/intersection.c
@@ -193,32 +193,118 @@ t_vector cone_normal(t_vector  p, t_figure *lst)
 }
 
 
-double cone_intersection(t_vector pos, t_vector dir, t_figure *lst)
+/*
+ Hit intersectCone(Cone s, Ray r)
+ {
+	 vec3 co = r.o - s.c;
+
+	 float a = dot(r.d,s.v)*dot(r.d,s.v) - s.cosa*s.cosa;
+	 float b = 2. * (dot(r.d,s.v)*dot(co,s.v) - dot(r.d,co)*s.cosa*s.cosa);
+	 float c = dot(co,s.v)*dot(co,s.v) - dot(co,co)*s.cosa*s.cosa;
+
+	 float det = b*b - 4.*a*c;
+	 if (det < 0.) return noHit;
+
+	 det = sqrt(det);
+	 float t1 = (-b - det) / (2. * a);
+	 float t2 = (-b + det) / (2. * a);
+
+	 // This is a bit messy; there ought to be a more elegant solution.
+	 float t = t1;
+	 if (t < 0. || t2 > 0. && t2 < t) t = t2;
+	 if (t < 0.) return noHit;
+
+	 vec3 cp = r.o + t*r.d - s.c;
+	 float h = dot(cp, s.v);
+	 if (h < 0. || h > s.h) return noHit;
+
+	 vec3 n = normalize(cp * dot(s.v, cp) / dot(cp, cp) - s.v);
+
+	 return Hit(t, n, s.m);
+ }
+ */
+
+double cone_intersection(t_vector origin, t_vector direction, t_figure *lst)
 {
-	double A = pos.x - lst->figure.co.center.x;
-	double B = pos.z - lst->figure.co.center.z;
-	double D = lst->figure.co.height - pos.y + lst->figure.co.center.y;
-	
-	double tan = (lst->figure.co.radius / lst->figure.co.height) * (lst->figure.co.radius / lst->figure.co.height);
-	
-	float a = (dir.x * dir.x) + (dir.z * dir.z) - (tan*(dir.y * dir.y));
-	float b = (2*A*dir.x) + (2*B*dir.z) + (2*tan*D*dir.y);
-	float c = (A*A) + (B*B) - (tan*(D*D));
-	
+//	t_vector co = vector_sub(origin, lst->figure.co.center);
+//
+//	float a = vector_dot(direction, lst->figure.co.nv)*_vector_dot(direction,lst->figure.co.nv) - s.cosa*s.cosa;
+//	float b = 2. * (vector_dot(direction,s.v)*vector_dot(co,s.v) - dot(r.d,co)*s.cosa*s.cosa);
+//	float c = dot(co,s.v)*dot(co,s.v) - dot(co,co)*s.cosa*s.cosa;
+//
+//	float det = b*b - 4.*a*c;
+//	if (det < 0.) return noHit;
+//
+//	det = sqrt(det);
+//	float t1 = (-b - det) / (2. * a);
+//	float t2 = (-b + det) / (2. * a);
+//
+//	// This is a bit messy; there ought to be a more elegant solution.
+//	float t = t1;
+//	if (t < 0. || t2 > 0. && t2 < t) t = t2;
+//	if (t < 0.) return noHit;
+//
+//	vec3 cp = r.o + t*r.d - s.c;
+//	float h = dot(cp, s.v);
+//	if (h < 0. || h > s.h) return noHit;
+//
+//	vec3 n = normalize(cp * dot(s.v, cp) / dot(cp, cp) - s.v);
+//
+//	return Hit(t, n, s.m);
+//	double A = origin.x - lst->figure.co.center.x;
+//	double B = origin.z - lst->figure.co.center.z;
+//	double D = lst->figure.co.height - origin.y + lst->figure.co.center.y;
+//	double theta = atan(lst->figure.co.radius / lst->figure.co.height);
+//	t_vector C = vector_sum(lst->figure.co.center, vector_mlt(lst->figure.co.height, lst->figure.co.nv));
+//	//double tan = (lst->figure.co.radius / lst->figure.co.height) * (lst->figure.co.radius / lst->figure.co.height);
+//	//dir = vector_dot(dir, lst->figure.co.nv);
+//	//float a = (dir.x * dir.x) + (dir.z * dir.z) - (tan*(dir.y * dir.y));
+//	float a = vector_dot(direction, lst->figure.co.nv) * vector_dot(direction, lst->figure.co.nv) - cos(theta) * cos(theta);
+//	//float b = (2*A*dir.x) + (2*B*dir.z) + (2*tan*D*dir.y);
+//	t_vector CO = vector_sub(origin, C);
+//	float b = 2 * (vector_dot(direction, lst->figure.co.nv) * vector_dot(CO, lst->figure.co.nv) - vector_dot(direction,CO) * cos(theta)*cos(theta));
+//
+//	float c = vector_dot(CO, lst->figure.co.nv) * vector_dot(CO, lst->figure.co.nv) - vector_dot(CO, CO) * vector_dot(CO, CO) * cos(theta) * cos(theta);
+//	//= (A*A) + (B*B) - (tan*(D*D));
+//
+//	float delta = b*b - 4*(a*c);
+//	if(fabs(delta) < 0.001) return -1.0;
+//	if(delta < 0.0) return -1.0;
+//
+//	float t1 = (-b - sqrt(delta))/(2*a);
+//	float t2 = (-b + sqrt(delta))/(2*a);
+//	float t;
+//
+//	if (t1>t2) t = t2;
+//	else t = t1;
+//	lst->normal = cone_normal(origin, lst);
+//	float r = origin.y + t*direction.y;
+//	printf("%f",t);
+//	if ((r > lst->figure.co.center.y) && (r < lst->figure.co.center.y + lst->figure.co.height)) return t;
+//	else return -1;
+	double theta = atan(lst->figure.co.radius / lst->figure.co.height);
+	t_vector C = vector_sum(lst->figure.co.center, vector_mlt(lst->figure.co.height, lst->figure.co.nv));
+	float a = vector_dot(direction, lst->figure.co.nv) * vector_dot(direction, lst->figure.co.nv) - cos(theta) * cos(theta);
+	t_vector CO = vector_sub(origin, C);
+	float b = 2 * (vector_dot(direction, lst->figure.co.nv) * vector_dot(CO, lst->figure.co.nv) - vector_dot(direction,CO) * cos(theta)*cos(theta));
+	float c = vector_dot(CO, lst->figure.co.nv) * vector_dot(CO, lst->figure.co.nv) - vector_dot(CO, CO) * vector_dot(CO, CO) * cos(theta) * cos(theta);
+
 	float delta = b*b - 4*(a*c);
 	if(fabs(delta) < 0.001) return -1.0;
 	if(delta < 0.0) return -1.0;
-	
+
 	float t1 = (-b - sqrt(delta))/(2*a);
 	float t2 = (-b + sqrt(delta))/(2*a);
 	float t;
-	
+
 	if (t1>t2) t = t2;
 	else t = t1;
-	lst->normal = cone_normal(pos, lst);
-	float r = pos.y + t*dir.y;
+	lst->normal = cone_normal(origin, lst);
+	float r = origin.y + t*direction.y;
 	printf("%f",t);
 	if ((r > lst->figure.co.center.y) && (r < lst->figure.co.center.y + lst->figure.co.height)) return t;
 	else return -1;
+	
+	//double
 }
 
