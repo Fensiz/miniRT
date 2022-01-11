9a928923e8b73ea78b20e6a80a5eaac3900ae377
Fensiz <fensiz@me.com>
2022-01-11 13:51:41 +0300

    cone without shadows

--- miniRT_v2/miniRT_v2/srcs/intersection.c
+++ miniRT_v2/miniRT_v2/srcs/intersection.c
@@ -185,59 +185,84 @@ double			cylinder_intersection(t_vector o, t_vector d, t_figure *lst)
 	return (INFINITY);
 }
 
-
-static double	caps_cone_intersection(t_vector o, t_vector d, t_figure *lst)
+static double	cap_intersection(t_vector o, t_vector d, t_figure *lst)
 {
 	double	id1;
 	t_vector	ip1;
 
-	id1 = solve_plane(o, d, lst->figure.cy.center, lst->figure.cy.nv);
+	id1 = solve_plane(o, d, lst->figure.co.center, lst->figure.co.nv);
 	if (id1 < INFINITY)
 	{
 		ip1 = vector_sum(o, vector_mlt(id1, d));
-		if (id1 < INFINITY && distance(ip1, lst->figure.cy.center) <= lst->figure.cy.radius)
+		if (id1 < INFINITY && distance(ip1, lst->figure.co.center) <= lst->figure.co.radius)
 			return (id1);
 		return (INFINITY);
 	}
 	return (INFINITY);
 }
-double cone_solve(t_vector origin, t_vector direction, t_figure *lst)
+
+
+
+double cone_solve(t_vector origin, t_vector direction, t_vector *normal, t_figure *lst)
 {
 	t_vector center = vector_sum(lst->figure.co.center, vector_mlt(lst->figure.co.height, lst->figure.co.nv));
 	t_vector co = vector_sub(origin, center);
 	t_vector v = vector_mlt(-1, lst->figure.co.nv);
 	double theta = atan(lst->figure.co.radius / lst->figure.co.height);
-	double a = pow(vector_dot(direction, v), 2) - pow(cos(theta), 2);
-	double b = 2. * (vector_dot(direction, v) * vector_dot(co, v) - vector_dot(direction, co) * pow(cos(theta), 2));
-	double c = pow(vector_dot(co, v), 2) - vector_dot(co,co) * pow(cos(theta), 2);
+	double	k[3];
+	double	x2[2];
 
-	double det = b*b - 4.*a*c;
-	if (det < 0.) return (INFINITY);
+	k[0] = pow(vector_dot(direction, v), 2) - pow(cos(theta), 2);
+	k[1] = 2 * (vector_dot(direction, v) * vector_dot(co, v) - vector_dot(direction, co) * pow(cos(theta), 2));
+	k[2] = pow(vector_dot(co, v), 2) - vector_dot(co,co) * pow(cos(theta), 2);
+	
+	double det = pow(k[1], 2) - 4 * k[0] * k[2];
+	if (det < 0)
+		return (INFINITY);
 
-	double t1 = (-b - sqrt(det)) / (2. * a);
-	double t2 = (-b + sqrt(det)) / (2. * a);
+	x2[0] = (-k[1] - sqrt(det)) / (2 * k[0]);
+	x2[1] = (-k[1] + sqrt(det)) / (2 * k[0]);
 
 	// This is a bit messy; there ought to be a more elegant solution.
-	double t = t1;
-	if (t < EPSILON || (t2 > EPSILON && t2 < t)) t = t2;
-	if (t < EPSILON) return (INFINITY);
+	double x = x2[0];
+	if (x < EPSILON || (x2[1] > EPSILON && x2[1] < x)) x = x2[1];
+	if (x < EPSILON) return (INFINITY);
 
-	t_vector cp = vector_sub(vector_sum(origin, vector_mlt(t, direction)), center);
+	t_vector cp = vector_sub(vector_sum(origin, vector_mlt(x, direction)), center);
 	double h = vector_dot(cp, v);
 	if (h < EPSILON || h > lst->figure.co.height) return (INFINITY);
 
-	lst->normal = vector_norm(vector_sub(vector_mlt(vector_dot(v, cp) / vector_dot(cp, cp), cp), v));
-	return t;
+	*normal = vector_norm(vector_sub(vector_mlt(vector_dot(v, cp) / vector_dot(cp, cp), cp), v));
+	return x;
 
 }
+static double	co_intersection(t_vector origin, t_vector direction, t_vector *normal, t_figure *lst)
+{
+	return cone_solve(origin, direction, normal, lst);
+//	double	x2[2];
+//
+//	if (solve_cone(x2, o, d, lst) == 0)
+//		return (INFINITY);
+//	lst->figure.cy.dist1 = vector_dot(lst->figure.cy.nv, vector_sub(vector_mlt(x2[0], d),
+//												vector_sub(lst->figure.cy.center, o)));
+//	lst->figure.cy.dist2 = vector_dot(lst->figure.cy.nv, vector_sub(vector_mlt(x2[1], d),
+//												vector_sub(lst->figure.cy.center, o)));
+//	if (!((lst->figure.cy.dist1 >= 0 && lst->figure.cy.dist1 <= lst->figure.cy.height
+//					&& x2[0] > EPSILON) || (lst->figure.cy.dist2 >= 0
+//					&& lst->figure.cy.dist2 <= lst->figure.cy.height && x2[0] > EPSILON)))
+//		return (INFINITY);
+//	*normal = calc_cy_normal(x2, o, d, lst);
+//	return (x2[0]);
+}
+
 double cone_intersection(t_vector origin, t_vector direction, t_figure *lst)
 {
 	double		cone_inter;
 	double		cap_inter;
 	t_vector	co_normal;
 
-	cone_inter = cy_intersection(origin, direction, &co_normal, lst);
-	cap_inter = caps_intersection(origin, direction, lst);
+	cone_inter = co_intersection(origin, direction, &co_normal, lst);
+	cap_inter = cap_intersection(origin, direction, lst);
 	if (cone_inter < INFINITY || cap_inter < INFINITY)
 	{
 		if (cone_inter < cap_inter)
@@ -250,6 +275,6 @@ double cone_intersection(t_vector origin, t_vector direction, t_figure *lst)
 	}
 	return (INFINITY);
 	
-	return cone_solve(origin, direction, lst);
+	//return cone_solve(origin, direction, lst);
 }
 
