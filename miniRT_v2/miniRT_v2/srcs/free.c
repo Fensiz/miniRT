7e5911c7dc3ed4f6f5f39a9d55d54aea047e96d6
Fensiz <fensiz@me.com>
2022-01-21 17:09:19 +0300

    norm

---
+++ miniRT_v2/miniRT_v2/srcs/free.c
@@ -0,0 +1,8 @@
+//
+//  free.c
+//  miniRT_v2
+//
+//  Created by Евгений on 21.01.2022.
+//
+
+#include <stdio.h>
---
+++ miniRT_v2/miniRT_v2/srcs/utils_4.c
@@ -0,0 +1,8 @@
+//
+//  utils_4.c
+//  miniRT_v2
+//
+//  Created by Евгений on 21.01.2022.
+//
+
+#include <stdio.h>
